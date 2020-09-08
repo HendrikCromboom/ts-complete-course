65ab9a0842820784ceb6bc6d6b300f0e6a6a0943
HendrikCromboom <hcromboom@gmail.com>
2020-09-08 20:25:16 +0200

    number solved and init

---
+++ 1. Intro/1. Number/script.ts
@@ -0,0 +1,9 @@
+function add(n1:number, n2:number){
+    return n1+n2;
+}
+const number1 = "7.1"
+const number2 = "8.9"
+
+const result:number = add(parseFloat(number1), parseFloat(number2))
+console.log(result)
+
