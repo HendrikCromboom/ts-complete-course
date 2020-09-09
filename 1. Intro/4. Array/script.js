d735cb1506611500c932d81ff794d20f64c6aa75
HendrikCromboom <hcromboom@gmail.com>
2020-09-09 14:44:19 +0200

    array init

---
+++ 1. Intro/4. Array/script.js
@@ -0,0 +1,7 @@
+var person = {
+    name: "Jan",
+    lastname: "Metdepet",
+    age: 28,
+    hobbies: ["Singing", "Dancing"]
+};
+console.log(person.hobbies);
---
+++ 1. Intro/4. Array/script.ts
@@ -0,0 +1,13 @@
+let person: {
+    name: string,
+    lastname: string,
+    age: number,
+    hobbies: string[]
+}={
+    name: "Jan",
+    lastname: "Metdepet",
+    age: 28,
+    hobbies: ["Singing", "Dancing"]
+}
+console.log(person.hobbies)
+
